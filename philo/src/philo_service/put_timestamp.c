143341523dee0c7542fd69ce42e88afc99cae7d5
yugo <yugo.hoshino1906@gmail.com>
2023-05-08 19:19:03 +0900

    clean: norm対応

--- philo/src/philo_service/put_timestamp.c
+++ philo/src/philo_service/put_timestamp.c
@@ -6,16 +6,17 @@
 /*   By: yughoshi <yughoshi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 11:18:23 by yughoshi          #+#    #+#             */
-/*   Updated: 2023/05/07 11:51:37 by yughoshi         ###   ########.fr       */
+/*   Updated: 2023/05/08 19:18:10 by yughoshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-void put_time_in_ms(struct timeval *tv)
+void	put_time_in_ms(struct timeval *tv)
 {
-	unsigned long timestamp;
+	unsigned long	timestamp;
+
 	gettimeofday(tv, NULL);
-    timestamp =  (tv->tv_sec * 1000) + (tv->tv_usec / 1000);
+	timestamp = (tv->tv_sec * 1000) + (tv->tv_usec / 1000);
 	printf("timestamp: %lu", timestamp);
 }
