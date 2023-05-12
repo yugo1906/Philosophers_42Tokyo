267ca49e32ea150e7f6ff55b0e92d0036ac63f0d
yugo <yugo.hoshino1906@gmail.com>
2023-05-13 05:11:29 +0900

    clean

--- philo/src/philo_service/error_handling.c
+++ philo/src/philo_service/error_handling.c
@@ -6,7 +6,7 @@
 /*   By: yughoshi <yughoshi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 19:30:27 by yughoshi          #+#    #+#             */
-/*   Updated: 2023/05/10 08:30:38 by yughoshi         ###   ########.fr       */
+/*   Updated: 2023/05/13 05:11:04 by yughoshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,15 @@ bool	put_error_end_exit(char *str)
 	return (EXIT_FAILURE);
 }
 
-bool	put_error_and_philo_free_exit(t_philo_env *p_env, char *s)
+bool	put_error_and_philo_free_exit(t_philo_env *p_env, char *str)
 {
 	free(p_env->philo);
-	return (put_error_end_exit(s));
+	return (put_error_end_exit(str));
 }
 
-bool	put_error_and_all_free_exit(t_philo_env *p_env, char *s)
+bool	put_error_and_all_free_exit(t_philo_env *p_env, char *str)
 {
 	free(p_env->philo);
 	free(p_env->fork);
-	return (put_error_end_exit(s));
+	return (put_error_end_exit(str));
 }
