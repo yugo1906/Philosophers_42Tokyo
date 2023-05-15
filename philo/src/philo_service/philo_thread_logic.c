205324b6b95e619d98dfe34dae50459e25da688d
yugo <yugo.hoshino1906@gmail.com>
2023-05-15 09:46:30 +0900

    update: mutexとスレッドのテスト。2人の哲学者の行動を交互に出力するプログラム

--- philo/src/philo_service/philo_thread_logic.c
+++ philo/src/philo_service/philo_thread_logic.c
@@ -6,44 +6,64 @@
 /*   By: yughoshi <yughoshi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 23:42:49 by yughoshi          #+#    #+#             */
-/*   Updated: 2023/05/13 05:12:49 by yughoshi         ###   ########.fr       */
+/*   Updated: 2023/05/15 09:45:13 by yughoshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-static void	philo_take_fork(t_philo *philo)
+static int	philo_take_fork(t_philo *philo)
 {
+	static int	i = 0;
+
 	pthread_mutex_lock(&philo->p_env->fork[philo->right_fork_id]);
+	usleep(100000);
 	put_philo_log(philo, "has taken a right_fork");
 	pthread_mutex_unlock(&philo->p_env->fork[philo->right_fork_id]);
 	pthread_mutex_lock(&philo->p_env->fork[philo->left_fork_id]);
 	put_philo_log(philo, "has taken a left_fork");
 	pthread_mutex_unlock(&philo->p_env->fork[philo->left_fork_id]);
+	i++;
+	printf("i = %d\n", i);
+	return (i);
 }
 
 static void	*philo_routine(void *a_philosopher)
 {
 	t_philo	*philo;
+	int		end_flag;
 
+	end_flag = 0;
 	philo = (t_philo *)a_philosopher;
-	philo_take_fork(philo);
+	while (1)
+	{
+		end_flag = philo_take_fork(philo);
+		if (end_flag == 10 || end_flag == 20)
+		{
+			printf("きた?\n");
+			break ;
+		}
+	}
 	return (NULL);
 }
 
 bool	create_philo_thread(t_philo_env *p_env)
 {
-	pthread_t		thread;
+	pthread_t		thread[p_env->num_of_philo];
 	unsigned int	i;
 
 	i = 0;
 	while (i < p_env->num_of_philo)
 	{
-		if (pthread_create(&thread, NULL, philo_routine, &p_env->philo[i]) != 0)
+		if (pthread_create(&thread[i], NULL, philo_routine,
+				&p_env->philo[i]) != 0)
 			return (put_error_and_all_free_exit(p_env, "philo_create_thread."));
-		usleep(500);
-		if (pthread_detach(thread))
-			return (put_error_and_all_free_exit(p_env, "philo_detach_thread."));
+		i++;
+	}
+	i = 0;
+	while (i < p_env->num_of_philo)
+	{
+		pthread_join(thread[i], NULL);
 		i++;
 	}
 	return (NOT_ERROR);
